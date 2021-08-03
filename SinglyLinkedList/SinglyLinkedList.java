bf08d0736f441263dd3da6ffa9bc281393ca9137
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2021-08-03 11:06:03 +0000

    modified SinglyLinkedList

--- SinglyLinkedList/SinglyLinkedList.java
+++ SinglyLinkedList/SinglyLinkedList.java
@@ -2,28 +2,8 @@ import java.util.Collection;
 
 class SinglyLinkedList<E> {
     Node<E> head; // head of list
-    Node<E> tail; //tail
+    Node<E> tail;
     long size = 0;
-
-    static class Node<E> {
-        E element;
-        Node next;
-
-        public Node(E element) {
-            this.element = element;
-            next = null;
-        }
-
-        public Object getElement(){
-            return element;
-        }
-
-        public Node<E> getNext() {
-            return next;
-        }
-
-    }
-
 
     public long getSize() {
         return size;
@@ -46,7 +26,7 @@ class SinglyLinkedList<E> {
 
     public void addFirst(E new_data) {
 
-        Node<E> new_node = new Node<E>(new_data);
+        Node<E> new_node = new Node<>(new_data);
         if (head == null) {
             new_node.next = null;
             head = new_node;
@@ -57,26 +37,61 @@ class SinglyLinkedList<E> {
         new_node.next = head;
         head = new_node;
         size++;
-
     }
 
-    public void insertAfter(Node<E> prev_node, E new_data) {
+    public void addFirst(Node<E> new_node) {
+        if (head == null) {
+            new_node.next = null;
+            head = new_node;
+            tail = new_node;
+            size++;
+        }
 
+        new_node.next = head;
+        head = new_node;
+        size++;
+    }
+
+    /* Inserts a new node after the given prev_node. */
+    public void insertAfter(Node<E> prev_node, E new_data) {
         Node<E> new_node = new Node<>(new_data);
         if (prev_node == null) {
             addFirst(new_data);
-        }else if(prev_node == tail){
+        } else if (prev_node == tail) {
             addLast(new_data);
-        }else{
-            Node<E> next= prev_node.next;
+        } else {
+            Node<E> next = prev_node.next;
             prev_node.next = new_node;
             new_node.next = next;
         }
     }
 
+    public void insertAfter(Node<E> prev_node, Node<E> new_node) {
+        if (prev_node == null) {
+            addFirst(new_node);
+        } else if (prev_node == tail) {
+            addLast(new_node);
+        } else {
+            Node<E> next = prev_node.next;
+            prev_node.next = new_node;
+            new_node.next = next;
+        }
+    }
 
-    public void addLast(E new_data) {
-        Node<E> new_node = new Node<E>(new_data);
+        Node<E> new_node = new Node<>(new_data);
+        new_node.next = null;
+        if (head == null) {
+            head = new_node;
+            tail = new_node;
+            size++;
+        }
+
+        tail.next = new_node;
+        tail = new_node;
+        size++;
+    }
+
+    public void addLast(Node<E> new_node) {
         new_node.next = null;
         if (head == null) {
             head = new_node;
@@ -104,32 +119,30 @@ class SinglyLinkedList<E> {
     }
 
     public void removeLast() {
-        if (tail == null){
+        if (tail == null) {
             return;
-        }
-        else{
-            if (head == tail){
+        } else {
+            if (head == tail) {
                 head = null;
                 tail = null;
-            }else{
+            } else {
                 Node<E> prevToTail = head;
-                while (prevToTail.next != tail){
+                while (prevToTail.next != tail) {
                     prevToTail = prevToTail.next;
                 }
                 tail = prevToTail;
                 tail.next = null;
             }
-	    size--;
+            size--;
         }
     }
 
     public E getFirst() {
-        return (int) head.element;
+        return head.element;
     }
 
     public E getLast() {
-
-        return (int) tail.element;
+        return tail.element;
     }
 
     public E get(int index)
@@ -151,28 +164,42 @@ class SinglyLinkedList<E> {
         return null;
     }
 
-
     public void clear() {
 
-        Node i = head;
-        while (i != null) {
+        Node<E> current = head;
+        while (current != null) {
             removeFirst();
-            i = i.next;
+            current = current.next;
         }
     }
 
-
-
     public void printList() {
-        Node n = head;
+        Node<E> n = head;
         while (n != null) {
             System.out.print(n.element + "->");
             n = n.next;
         }
+        System.out.println("null");
+    }
+
+    /* Linked list Node*/
+    static class Node<E> {
+        E element;
+        Node<E> next;
+
+        public Node(E element) {
+            this.element = element;
+            next = null;
+        }
+
+        public Object getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+
     }
 }
-
-
-
-
-
